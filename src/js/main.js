f2adfff13022682aca316bf6ddb47a473046c97c
iDevYT <alphangred57@gmail.com>
2022-05-26 13:50:51 +0200

    the biggest change as of now

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+
+var btn = document.getElementById('cta');
+btn.onclick = () => {
+    window.location.href = '/src/html/contact.html';
+};
---
+++ src/js/main.ts
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btn');
+
+btn.onclick = () => {
+    alert("Hello World!");
+}
